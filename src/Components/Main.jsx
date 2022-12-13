a5bbfa7a01d885e6541a0d5dce04441b1a9c52e5
Hasnain Syed <hasnainalsyed@gmail.com>
2022-12-13 20:04:55 +0500

    Initial Commit

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Benefits from "./Benefits";
+import SocialIcons from "./SocialIcons";
 
 const Main = () => {
   return (
@@ -35,7 +37,7 @@ const Main = () => {
                 <div className="content">
                   <h3>THE EARLY CROW</h3>
                   <p>CRO CROW is the very first NFT on the Cronos Chain. </p>
-                  <p>Edition #1 was minted on block #946, just 1 hour and 27 minutes after Cronos was launched: <a href="#">source</a></p>
+                  <p>Edition #1 was minted on block #946, just 1 hour and 27 minutes after Cronos was launched: <a href="/">source</a></p>
                   <p>This is the main NFT of the CRO CROW ecosystem, and holders passively earn a share of the project profits via airdrops or royalty distribution.</p>
                 </div>
               </div>
@@ -69,8 +71,85 @@ const Main = () => {
                 <div className="content text-end">
                   <h3>CRONOS PUNK</h3>
                   <p>CRONOS PUNK was the second NFT deployed on the Cronos Chain. </p>
-                  <p>The contract was deployed 5 hours after CRO CROW, on block #4257: <a href="#">source</a></p>
+                  <p>The contract was deployed 5 hours after CRO CROW, on block #4257: <a href="/">source</a></p>
                   <p>This NFT eventually adopted by the CRO CROW team, since the original team became inactive and the community wanted to move forward..</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="row first-line">
+              <div className="col-12">
+                <div class="group">
+                  <hr
+                    aria-orientation="horizontal"
+                    class="chakra-divider css-44cso2"
+                  />
+                  <img src="images/crow-1.png" alt="Song Crow" className="img-fluid" />
+                  <img src="images/crow-2.png" alt="Song Crow" className="img-fluid" />
+                </div>
+              </div>
+            </div>
+
+            <Benefits />
+
+            <div className="row second-line">
+              <div className="col-12">
+                <div class="group">
+                  <hr
+                    aria-orientation="horizontal"
+                    class="chakra-divider css-44cso2"
+                  />
+                  <img src="images/crow-1.png" alt="Song Crow" className="img-fluid" />
+                  <img src="images/crow-2.png" alt="Song Crow" className="img-fluid" />
+                </div>
+              </div>
+            </div>
+
+            <div className="row team justify-content-center">
+              <div className="col-12">
+                <div className="heading text-center">
+                  <h2>THE CRO CROW TEAM   </h2>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <a href="www.twitter.com" className="team-member" target='_blank' rel="noreferrer">
+                  <img src="/images/destruction.png" alt="Destruction" className="img-fluid" />
+                  <h4>@destructioneth</h4>
+                  <p>Founder and Dev</p>
+                </a>
+              </div>
+              <div className="col-md-4">
+                <a href="www.twitter.com" className="team-member" target='_blank' rel="noreferrer">
+                  <img src="/images/alemf.png" alt="Alemf" className="img-fluid" />
+                  <h4>@ale_nf</h4>
+                  <p>Artist & Design</p>
+                </a>
+              </div>
+              <div className="col-md-4">
+                <a href="www.twitter.com" className="team-member" target='_blank' rel="noreferrer">
+                  <img src="/images/blueaxolotl.png" alt="Blueaxolotl" className="img-fluid" />
+                  <h4>@BlueAxolotlETH</h4>
+                  <p>Community Manager</p>
+                </a>
+              </div>
+              <div className="col-md-4">
+                <a href="www.twitter.com" className="team-member" target='_blank' rel="noreferrer">
+                  <img src="/images/joel.png" alt="Joel" className="img-fluid" />
+                  <h4>@Joel_</h4>
+                  <p>Discord Mod</p>
+                </a>
+              </div>
+            </div>
+
+            <div className="row first-line">
+              <div className="col-12">
+                <div class="group">
+                  <hr
+                    aria-orientation="horizontal"
+                    class="chakra-divider css-44cso2"
+                  />
+                  <img src="images/crow-1.png" alt="Song Crow" className="img-fluid" />
+                  <img src="images/crow-2.png" alt="Song Crow" className="img-fluid" />
                 </div>
               </div>
             </div>
@@ -78,6 +157,27 @@ const Main = () => {
           </div>
         </div>
       </section>
+
+      <footer>
+        <div className="img-wrapper">
+          <img src="/images/footer.gif" alt="Song Crows" className="img-fluid" />
+        </div>
+        <div className="footer-wrap">
+          <div className="container">
+            <div className="row align-items-center">
+              <div className="col-md-6">
+                <div className="img-fluid d-flex flex-row align-items-center">
+                  <img src="/logo.png" alt="Song Crows" className="img-fluid" />
+                  <p className="mb-0 ms-3 text-white lh-1 fs-3">contact@songcrows.com</p>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <SocialIcons />
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
